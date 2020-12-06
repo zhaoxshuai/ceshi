46427b0ad0b027ca3f8a4bc1afb8fbd6b491ba46
zhaoxshuai <746185810@qq.com>
2020-12-06 18:37:37 +0800

    首次提交

---
+++ src/main/java/com/example/ceshi/controller/CeshiController.java
@@ -0,0 +1,17 @@
+package com.example.ceshi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zxs
+ * @date 2020/12/6 下午6:32
+ */
+@RestController
+public class CeshiController {
+
+    @RequestMapping("/ceshi")
+    public String ceshi(){
+        return "hell word";
+    }
+}
